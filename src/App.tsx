63bb7fd7caa83c71a6af21ad5f101bb545fd6b05
Marko-batman <krznarm26@gmail.com>
2022-05-22 16:45:53 +0200

    Added dark theme & animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import TimeEntry from "./containers/TimeEntry/TimeEntry";
 
 const App: React.FC = () => {
+  const [theme, setTheme] = useState(false);
+
   return (
-    <>
+    <div className={theme ? "content dark-theme" : "content"}>
       <h1>Productive time tracker</h1>
+      <button
+        className="btn btn--toggle-theme"
+        onClick={() => setTheme(!theme)}
+      >
+        <span className="material-symbols-outlined">
+          {theme ? "light_mode" : "nightlight"}
+        </span>
+      </button>
       <TimeEntry />
-    </>
+    </div>
   );
 };
 
